f6ed9136c153db4431a5c96e27c923abc3e2e850
Alisina <99441567+imalisina@users.noreply.github.com>
2024-08-01 22:48:01 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "ibm-react-project",
+  base: "/ibm-react-project/",
   plugins: [react()],
 })
